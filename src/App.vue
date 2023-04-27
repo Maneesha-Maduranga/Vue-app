d94d9b8c258da1d803606edecd712bc32a8569e6
ManeeshaMaduranga <MadurangaBandara1006@gmail.com>
2023-04-27 20:21:38 +0530

    Vue Js Basic Done

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import Modal from './components/Modal.vue';
+</script>
+
+<template>
+  <Modal />
+</template>
+
+
---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    methods:{
+        toggle(){
+            this.$refs.btn.classList.add('none')
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="modal" ref="btn">
+        <button class="close" @click="toggle" >Close</button>
+        <h1>Modal Header</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet officia quisquam molestias et ullam odit totam error, modi consectetur saepe?</p>
+    </div>
+</template>
+
+<style>
+    .modal{
+        height: 150px;
+        width: 30%;
+        background-color:aliceblue;
+        color: black;
+        border-radius: 10px;
+        text-align: center;
+        padding: auto;
+        position: relative;
+        margin: auto;
+    }
+    .close{
+        position: absolute;
+        right: 25px;
+        top: 10px;
+        background-color: tomato;
+        color: #fff;
+        border-radius: 5px;
+    }
+    .close:hover{
+        background-color: #fff;
+    }
+    .none{
+        visibility: hidden;
+    }
+   
+</style>
